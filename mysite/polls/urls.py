1beeb8b19bd9d30ee738c0b73d3dc19213eb8a56
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-09-04 12:58:44 -0600

    Se genero una nueva vista usando plantillas (vista detalle)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,8 @@
 
 from django.urls import path
 
+from polls import views
+
 
 
 urlpatterns = [
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 from django.template import loader
@@ -17,7 +17,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get (pk=question_id)   
+    # except Question.DoesNotExist :
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
