8e0d7c59d9d8778dabea3369c2d7d6e5bf39d839
ElGivox <gi.vega@itsur.edu.mx>
2024-09-17 12:06:20 -0600

    Se adiciono vistas genericas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,25 @@ from django.urls import reverse
 from polls.models import Choice, Question
 from django.template import loader
 
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "lastest_question_list"
+
+    def get_queryset(self):
+        """Devuelve las últimas 5 publicadas"""
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 # Create your views here.
 def index(request):
     lastest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -43,4 +62,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    
